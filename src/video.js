bc5317da627a1610d310dbf5bd32bfa21f49acb0
jaeh <jascha@jaeh.at>
2021-05-25 00:45:11 +0200

    src/video: use bitrate from mp4 to calculate webm bitrate (60%), add codec check and file size check after build

--- src/video.js
+++ src/video.js
@@ -21,6 +21,46 @@ const fn = async () => {
 
     const fileName = path.basename(dir)
 
+    const mp4Path = `${dir}/${fileName}.mp4`
+
+    const mp4Info = child_process.execSync(`mediainfo --Output=JSON ${mp4Path}`)
+    const parsedInfo = JSON.parse(mp4Info)
+
+    const mp4Options = {}
+
+    parsedInfo.media.track.forEach(asset => {
+      if (asset['@type'] === 'Video') {
+        const { BitRate, Format, CodecID } = asset
+
+        if (Format !== 'AVC') {
+          throw new Error(`MP4 Video Error: Format is expected to be AVC, got ${Format}`)
+        }
+
+        if (CodecID !== 'avc1') {
+          throw new Error(`MP4 Video Error: CodecID is expected to be avc1, got ${CodecID}`)
+        }
+
+        mp4Options.video = {
+          BitRate,
+        }
+      } else if (asset['@type'] === 'Audio') {
+        const { BitRate, SamplingRate, Format, CodecID } = asset
+
+        if (Format !== 'AAC') {
+          throw new Error(`MP4 Audio Error: Format is expected to be AAC, got ${Format}`)
+        }
+
+        if (CodecID !== 'mp4a-40-2') {
+          throw new Error(`MP4 Video Error: CodecID is expected to be mp4a-40-2, got ${CodecID}`)
+        }
+
+        mp4Options.audio = {
+          BitRate,
+          SamplingRate,
+        }
+      }
+    })
+
     const webmPath = `${dir}/${fileName}.webm`
     const webmExists = fs.existsSync(webmPath)
 
@@ -28,21 +68,43 @@ const fn = async () => {
       return
     }
 
+    // const { BitRate, SamplingRate } = parsedInfo.media.track[1]
+    // const bitrate = findClosest(BitRate / 1000, [64, 128, 160, 192])
+    // const samplingRate = findClosest(SamplingRate, [44100, 48000])
+
+    // console.log({ BitRate, SamplingRate })
+
+    const videoBitRate = Math.floor(mp4Options.video.BitRate * 0.6)
 
     const args = [
       '-i', `${dir}/${fileName}${srcExt}`,
       '-c:v', 'libvpx',
-      '-crf', '50',
-      '-b:v', '1M',
-      '-c:a', 'libvorbis',
-    ].join(' ')
+      '-b:v', videoBitRate,
+    ]
+
+    if (mp4Options.audio) {
+      const audioArgs = ['-c:a', 'libvorbis']
+      args.push(...audioArgs)
+    }
 
     const output = `./docs/video/${fileName}/${fileName}.webm`
 
     log('Converting', output)
-    child_process.execSync(`${cmd} ${args} ${output}`)
+    const ffmpeg = `${cmd} ${args.join(' ')} ${output}`
+    console.warn('exec', ffmpeg)
+    child_process.execSync(ffmpeg)
     log.success('done')
   })
+
+  const videoFiles = await fs.getFiles(videoDir)
+
+  await Promise.all(videoFiles.map(async video => {
+    const stat = await fs.stat(video)
+
+    if (stat.size === 0) {
+      log.error('E_EMPTY_FILE', `Empty Video File: ${video}`)
+    }
+  }))
 }
 
 fn()
